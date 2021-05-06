e117931fb9eacb52ff0ad9937f4bad3f16ddff7d
Vrinda Tyagi <vrindatyagi09@gmail.com>
2021-05-06 17:57:24 +0530

    added auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+
+import { ProviderAuth } from "../lib/auth"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(<ProviderAuth>
+   <Component {...pageProps} />
+  </ProviderAuth>)
 }
 
 export default MyApp
